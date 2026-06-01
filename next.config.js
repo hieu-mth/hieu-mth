9704bf99a154f7c4a7a07aa697594cec19e37256
Mai Tran Huu Hieu <mthh.shun.1804@gmail.com>
2026-06-01 21:16:43 +0700

    fix vercel config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/hieu-mth',
-  assetPrefix: '/hieu-mth/',
   images: {
     unoptimized: true,
   },
